354ceeee37712fcde1f145c8350d348b52552baa
Meng Cao <meca7653@gmail.com>
2018-08-28 18:59:58 -0600

    change the n_lambda value

--- R/cv_fastclime.R
+++ R/cv_fastclime.R
@@ -6,7 +6,7 @@ cv_fastclime <- function(X, fold = 5, lambda = seq(1e-4, 1e-2, length.out = 50))
   res_cv <- foreach(ii = c(1:fold), .combine = "rbind")%do%{
     ind_train <- c(1:n)[-c(c(1:floor(n/fold)) + (ii - 1) * (n/fold))]
     train <- X[ind_train,]
-    L <- fastclime(train, lambda.min = 1e-7, nlambda = 800)
+    L <- fastclime(train, lambda.min = 1e-7, nlambda = 3000)
     test <- X[c(c(1:floor(n/fold)) + (ii - 1) * (n/fold)),]
     res_pre <- foreach(ll = lambda, .combine = "cbind")%do%{
       out2 <- fastclime.selector(L$lambdamtx, L$icovlist,ll)
@@ -16,7 +16,7 @@ cv_fastclime <- function(X, fold = 5, lambda = seq(1e-4, 1e-2, length.out = 50))
     res_pre
   }
   la <- lambda[which.min(apply(res_cv, 2, mean))]
-  L_final <- fastclime(X, lambda.min = 1e-7, nlambda = 800)
+  L_final <- fastclime(X, lambda.min = 1e-7, nlambda = 3000)
   out <- fastclime.selector(L_final$lambdamtx, L_final$icovlist,la)
   M_inv_final <- out$icov
   return(M_inv_final)
